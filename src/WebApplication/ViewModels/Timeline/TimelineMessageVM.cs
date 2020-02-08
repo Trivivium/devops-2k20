ec92c76da45d5780674579b92355fc559676ba89
Casper Weiss Bang <master@thecdk.net>
2020-02-08 08:44:56 +0100

    Implemented the gravatar api

--- src/WebApplication/ViewModels/Timeline/TimelineMessageVM.cs
+++ src/WebApplication/ViewModels/Timeline/TimelineMessageVM.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Text;
+using System.Security.Cryptography;
 
 namespace WebApplication.ViewModels.Timeline
 {
@@ -22,10 +24,27 @@ namespace WebApplication.ViewModels.Timeline
 
         public string GetGravatarURL(int size = 80)
         {
-            // Python: (md5(email.strip().lower().encode('utf-8')).hexdigest()
-            var hash = "";
+            var hash = CalculateMD5Hash(User.Email);
             
-            return $"http://www.gravatar.com/avatar/%{hash}?d=identicon&s=%{size}";
+            return $"http://www.gravatar.com/avatar/{hash}?d=identicon&s={size}";
+        }
+        public string CalculateMD5Hash(string input)
+        {
+            // we want to remove any wild spaces etc.
+            input = input.Trim().ToLower();
+
+            // calculate MD5 hash from input
+            MD5 md5 = System.Security.Cryptography.MD5.Create();
+            byte[] inputBytes = System.Text.Encoding.ASCII.GetBytes(input);
+            byte[] hash = md5.ComputeHash(inputBytes);
+
+            // convert byte array to hex string
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < hash.Length; i++)
+            {
+                sb.Append(hash[i].ToString("X2"));
+            }
+            return sb.ToString();
         }
     }
 }
