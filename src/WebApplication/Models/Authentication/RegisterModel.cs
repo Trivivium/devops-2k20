4f4ae6854298b1040264c639c5103d106ff1cc65
andersbjergfelt <andersbjergfelt@users.noreply.github.com>
2020-02-09 16:36:01 +0100

    recreated registration view and implemented registration. Might need to redirect to another page. We'll see.

---
+++ src/WebApplication/Models/Authentication/RegisterModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace WebApplication.Models.Authentication
+{
+    public class RegisterModel
+    {
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string PasswordHash { get; set; }
+    }
+}
