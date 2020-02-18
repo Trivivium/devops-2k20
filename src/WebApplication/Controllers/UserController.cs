a6429371db601e12808a59a77c80df30ed7f0959
Casper Weiss Bang <master@thecdk.net>
2020-02-18 12:05:24 +0100

    removed endpoints that are defined elsewhere

--- src/WebApplication/Controllers/UserController.cs
+++ src/WebApplication/Controllers/UserController.cs
@@ -7,16 +7,5 @@ namespace WebApplication.Controllers
     [Authorize]
     public class UserController : Controller
     {
-        [HttpPost("/{username}/follow")]
-        public IActionResult Follow(string username)
-        {
-            throw new NotImplementedException();
-        }
-        
-        [HttpPost("/{username}/unfollow")]
-        public IActionResult Unfollow(string username)
-        {
-            throw new NotImplementedException();
-        }
     }
 }
