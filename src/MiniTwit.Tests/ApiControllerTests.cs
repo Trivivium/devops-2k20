0420bf7010b129f9d6ada021abf6acf2d3b7160b
andersbjergfelt <andersbjergfelt@users.noreply.github.com>
2020-02-27 23:53:17 +0100

    fiddling with tests

--- src/MiniTwit.Tests/ApiControllerTests.cs
+++ src/MiniTwit.Tests/ApiControllerTests.cs
@@ -5,42 +5,97 @@ using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using WebApplication;
 using WebApplication.Entities;
 using WebApplication.Models.Authentication;
 using Xunit;
+using Xunit.Abstractions;
 
 namespace MiniTwit.Tests
 {
-    public class ApiControllerTests 
+    public class ApiControllerTests : IClassFixture<CustomWebApplicationFactory<Startup>>
     {
 
         //http://*:5000
 
-        private string _url = "http://localhost:11501";
+        //private string _url = "http://localhost:11501";
 
-        public ApiControllerTests()
+        private readonly CustomWebApplicationFactory<Startup> _factory;
+
+        private readonly ITestOutputHelper _output;
+
+        private  HttpClient _client;
+
+        public ApiControllerTests(CustomWebApplicationFactory<Startup> factory, ITestOutputHelper output)
         {
-           
+            _factory = factory;
+            _client = factory.CreateClient(new WebApplicationFactoryClientOptions
+            {
+                AllowAutoRedirect = false
+            });
+            _output = output;
+
+
         }
 
 
-        [Fact]
+        [Theory]
+        [InlineData("/")]
+        [InlineData("/login")]
+        public async Task Get_Endpoints(string url)
+        {
+            _client = _factory.CreateClient();
+
+            var response = await _client.GetAsync(url);
+
+            response.EnsureSuccessStatusCode();
+            Assert.Equal("text/html; charset=utf-8",
+            response.Content.Headers.ContentType.ToString());
+        }
+
+        
+       // [Fact]
         public async Task TestLatest()
         {
-            var client = new HttpClient();
+            
 
             var data = new { Email = "test@test",Password = "foo", Username = "test"};
 
             var jsonContent = JsonConvert.SerializeObject(data);
 
-            var httpResponse = await client.PostAsync($"{_url}/api/register", new StringContent(jsonContent, UnicodeEncoding.UTF8, "application/json"));
+            var httpResponse = await _client.PostAsync("/api/register", new StringContent(jsonContent, UnicodeEncoding.UTF8, "application/json"));
 
-            Console.WriteLine(httpResponse.StatusCode);
 
-            Assert.True(httpResponse.StatusCode  == HttpStatusCode.BadRequest);
+            Assert.True(httpResponse.StatusCode  == HttpStatusCode.NoContent);
+
+            //get latest here
 
 
         }
+
+
+        [Fact]
+        public async Task Test_Register()
+        {
+            var data = new { Email = "a@a.a", Password = "a", Username = "a", Latest = 1};
+
+            var jsonContent = JsonConvert.SerializeObject(data);
+
+            var httpResponse = await _client.PostAsync("/api/register", new StringContent(jsonContent, UnicodeEncoding.UTF8, "application/json"));
+
+            var httpResponseLatest = await _client.GetAsync("/api/latest");
+
+
+            _output.WriteLine(httpResponseLatest.StatusCode.ToString());
+
+            //get latest here
+
+
+            Assert.True(httpResponse.StatusCode == HttpStatusCode.NoContent);
+
+        }
+
+
     }
 }
