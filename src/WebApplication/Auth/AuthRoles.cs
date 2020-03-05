2631345aad9c49fc91c36de65d9e49ffee455710
thomb <Trivivium@users.noreply.github.com>
2020-03-05 15:55:30 +0100

    Add registered and administrator roles
    The user with the username "admin" is the only with the
    administrator role.

---
+++ src/WebApplication/Auth/AuthRoles.cs
@@ -0,0 +1,8 @@
+namespace WebApplication.Auth
+{
+    public class AuthRoles
+    {
+        public const string Registered = "Registered";
+        public const string Administrator = "Administrator";
+    }
+}
