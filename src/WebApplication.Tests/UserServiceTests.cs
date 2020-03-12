be55ec33491ff74960c816f8e6065e54a209cb40
Thomas <Trivivium@users.noreply.github.com>
2020-03-12 17:12:42 +0100

    Add error-path tests to user service

--- src/WebApplication.Tests/UserServiceTests.cs
+++ src/WebApplication.Tests/UserServiceTests.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
@@ -5,6 +6,7 @@ using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 
 using WebApplication.Entities;
+using WebApplication.Exceptions;
 using WebApplication.Models.Authentication;
 using WebApplication.Services;
 
@@ -43,6 +45,56 @@ namespace WebApplication.Tests
                 Assert.NotEqual("a", user.Password);
             }
         }
+        
+        [Fact]
+        public async Task CreateUser_Throws_WhenUsernameAlreadyExists()
+        {
+            var options = new DbContextOptionsBuilder<DatabaseContext>()
+                .UseInMemoryDatabase(nameof(CreateUser_Throws_WhenUsernameAlreadyExists))
+                .Options;
+
+            await using (var dbContext = new DatabaseContext(options))
+            {
+                dbContext.Users.Add(new User { Username = "a", Email = "a@a.a", Password = "a" });
+                dbContext.SaveChanges();
+            }
+
+            await using (var dbContext = new DatabaseContext(options))
+            {
+                
+                await Assert.ThrowsAsync<CreateUserException>(async () => {
+                    var service = new UserService(dbContext);
+                    var model = new RegisterModel { Username = "a", Email = "a@a.a", Pwd = "a" };
+
+                    await service.CreateUser(model, CancellationToken.None);
+                });
+            }
+        }
+        
+        [Fact]
+        public async Task CreateUser_Throws_WhenEmailAlreadyExists()
+        {
+            var options = new DbContextOptionsBuilder<DatabaseContext>()
+                .UseInMemoryDatabase(nameof(CreateUser_Throws_WhenUsernameAlreadyExists))
+                .Options;
+
+            await using (var dbContext = new DatabaseContext(options))
+            {
+                dbContext.Users.Add(new User { Username = "a", Email = "a@a.a", Password = "a" });
+                dbContext.SaveChanges();
+            }
+
+            await using (var dbContext = new DatabaseContext(options))
+            {
+                
+                await Assert.ThrowsAsync<CreateUserException>(async () => {
+                    var service = new UserService(dbContext);
+                    var model = new RegisterModel { Username = "a", Email = "a@a.a", Pwd = "a" };
+
+                    await service.CreateUser(model, CancellationToken.None);
+                });
+            }
+        }
 
         [Fact]
         public async Task GetUserFromUsername_Works()
@@ -71,30 +123,38 @@ namespace WebApplication.Tests
         }
         
         [Fact]
+        public async Task GetUserFromUsername_Throws_WhenUserDoesNotExit()
+        {
+            var options = new DbContextOptionsBuilder<DatabaseContext>()
+                .UseInMemoryDatabase(nameof(GetUserFromUsername_Works))
+                .Options;
+            
+            await using (var dbContext = new DatabaseContext(options))
+            {
+                await Assert.ThrowsAsync<InvalidOperationException>(async () => 
+                {
+                    var service = new UserService(dbContext);
+                    
+                    await service.GetUserFromUsername("b", CancellationToken.None);
+                });
+            }
+        }
+        
+        [Fact]
         public async Task IsUserFollowing_Works()
         {
             var options = new DbContextOptionsBuilder<DatabaseContext>()
                 .UseInMemoryDatabase(nameof(IsUserFollowing_Works))
                 .Options;
-
-            // Seeding the database
+            
             await using (var dbContext = new DatabaseContext(options))
             {
-                dbContext.Users.Add(new User { ID = 1, Username = "a", Email = "a@a.a", Password = "a" });
-                dbContext.Users.Add(new User { ID = 2, Username = "b", Email = "b@b.b", Password = "b" });
-                dbContext.Users.Add(new User { ID = 3, Username = "c", Email = "c@c.c", Password = "c" });
-                
-                dbContext.Followers.Add(new Follower { WhoID = 1, WhomID = 2 });
-                
-                dbContext.SaveChanges();
-            }
+                await Assert.ThrowsAsync<InvalidOperationException>(async () => 
+                {
+                    var service = new UserService(dbContext);
 
-            await using (var dbContext = new DatabaseContext(options))
-            {
-                var service = new UserService(dbContext);
-                var result = await service.IsUserFollowing(2, "a", CancellationToken.None);
-                
-                Assert.True(result);
+                    await service.IsUserFollowing(2, "a", CancellationToken.None);
+                });
             }
         }
         
@@ -128,10 +188,39 @@ namespace WebApplication.Tests
         }
         
         [Fact]
-        public async Task AddFollower_Works()
+        public async Task GetUserFollowers_Throws_WhenUserDoesNotExist()
         {
             var options = new DbContextOptionsBuilder<DatabaseContext>()
-                .UseInMemoryDatabase(nameof(AddFollower_Works))
+                .UseInMemoryDatabase(nameof(GetUserFollowers_Throws_WhenUserDoesNotExist))
+                .Options;
+
+            // Seeding the database
+            await using (var dbContext = new DatabaseContext(options))
+            {
+                dbContext.Users.Add(new User { Username = "a", Email = "a@a.a", Password = "a" });
+                dbContext.Users.Add(new User { Username = "b", Email = "b@b.b", Password = "b" });
+                dbContext.Users.Add(new User { Username = "c", Email = "c@c.c", Password = "c" });
+                
+                dbContext.Followers.Add(new Follower { WhoID = 2, WhomID = 1 });
+                dbContext.Followers.Add(new Follower { WhoID = 3, WhomID = 1 });
+                
+                dbContext.SaveChanges();
+            }
+
+            await using (var dbContext = new DatabaseContext(options))
+            {
+                var service = new UserService(dbContext);
+                var result = await service.GetUserFollowers("a", 5, CancellationToken.None);
+                
+                Assert.Equal(2, result.Count);
+            }
+        }
+        
+        [Fact]
+        public async Task AddFollower_UsingUsername_Works()
+        {
+            var options = new DbContextOptionsBuilder<DatabaseContext>()
+                .UseInMemoryDatabase(nameof(AddFollower_UsingUsername_Works))
                 .Options;
 
             // Seeding the database
@@ -159,10 +248,125 @@ namespace WebApplication.Tests
         }
         
         [Fact]
-        public async Task RemoveFollower_Works()
+        public async Task AddFollower_UsingUsername_Throws_WhenUserToFollow_DoesNotExist()
         {
             var options = new DbContextOptionsBuilder<DatabaseContext>()
-                .UseInMemoryDatabase(nameof(RemoveFollower_Works))
+                .UseInMemoryDatabase(nameof(AddFollower_UsingUsername_Throws_WhenUserToFollow_DoesNotExist))
+                .Options;
+            
+            await using (var dbContext = new DatabaseContext(options))
+            {
+                await Assert.ThrowsAsync<InvalidOperationException>(async () =>
+                {
+                    var service = new UserService(dbContext);
+
+                    await service.AddFollower("a", "b", CancellationToken.None);
+                });
+            }
+        }
+        
+        [Fact]
+        public async Task AddFollower_UsingUsername_Throws_WhenUserFollowing_DoesNotExist()
+        {
+            var options = new DbContextOptionsBuilder<DatabaseContext>()
+                .UseInMemoryDatabase(nameof(AddFollower_UsingUsername_Throws_WhenUserFollowing_DoesNotExist))
+                .Options;
+            
+            await using (var dbContext = new DatabaseContext(options))
+            {
+                dbContext.Users.Add(new User { Username = "a", Email = "a@a.a", Password = "a" });
+                dbContext.SaveChanges();
+            }
+            
+            await using (var dbContext = new DatabaseContext(options))
+            {
+                await Assert.ThrowsAsync<InvalidOperationException>(async () =>
+                {
+                    var service = new UserService(dbContext);
+
+                    await service.AddFollower("a", "b", CancellationToken.None);
+                });
+            }
+        }
+                
+        [Fact]
+        public async Task AddFollower_UsingUserID_Works()
+        {
+            var options = new DbContextOptionsBuilder<DatabaseContext>()
+                .UseInMemoryDatabase(nameof(AddFollower_UsingUserID_Works))
+                .Options;
+
+            // Seeding the database
+            await using (var dbContext = new DatabaseContext(options))
+            {
+                dbContext.Users.Add(new User { Username = "a", Email = "a@a.a", Password = "a" });
+                dbContext.Users.Add(new User { Username = "b", Email = "b@b.b", Password = "b" });
+                
+                dbContext.SaveChanges();
+            }
+
+            await using (var dbContext = new DatabaseContext(options))
+            {
+                var service = new UserService(dbContext);
+                
+                await service.AddFollower(1, "b", CancellationToken.None);
+            }
+            
+            await using (var dbContext = new DatabaseContext(options))
+            {
+                var followers = dbContext.Followers.Where(row => row.WhomID == 1).ToList();
+                
+                Assert.Single(followers);
+            }
+        }
+        
+        [Fact]
+        public async Task AddFollower_UsingUserID_Throws_WhenUserToFollow_DoesNotExist()
+        {
+            var options = new DbContextOptionsBuilder<DatabaseContext>()
+                .UseInMemoryDatabase(nameof(AddFollower_UsingUserID_Throws_WhenUserToFollow_DoesNotExist))
+                .Options;
+            
+            await using (var dbContext = new DatabaseContext(options))
+            {
+                await Assert.ThrowsAsync<InvalidOperationException>(async () =>
+                {
+                    var service = new UserService(dbContext);
+
+                    await service.AddFollower(1, "b", CancellationToken.None);
+                });
+            }
+        }
+        
+        [Fact]
+        public async Task AddFollower_UsingUserID_Throws_WhenUserFollowing_DoesNotExist()
+        {
+            var options = new DbContextOptionsBuilder<DatabaseContext>()
+                .UseInMemoryDatabase(nameof(AddFollower_UsingUserID_Throws_WhenUserFollowing_DoesNotExist))
+                .Options;
+
+            await using (var dbContext = new DatabaseContext(options))
+            {
+                dbContext.Users.Add(new User { Username = "a", Email = "a@a.a", Password = "a" });
+                dbContext.SaveChanges();
+            }
+            
+            await using (var dbContext = new DatabaseContext(options))
+            {
+                await Assert.ThrowsAsync<InvalidOperationException>(async () =>
+                {
+                    var service = new UserService(dbContext);
+
+                    await service.AddFollower(1, "b", CancellationToken.None);
+                });
+            }
+        }
+        
+        [Fact]
+        public async Task RemoveFollower_UsingUsername_Works()
+        {
+            var options = new DbContextOptionsBuilder<DatabaseContext>()
+                .UseInMemoryDatabase(nameof(RemoveFollower_UsingUsername_Works))
                 .Options;
 
             // Seeding the database
@@ -188,6 +392,180 @@ namespace WebApplication.Tests
                 var followers = dbContext.Followers.Where(row => row.WhomID == 1).ToList();
                 
                 Assert.Empty(followers);
+            }
+        }
+        
+        [Fact]
+        public async Task RemoveFollower_UsingUsername_Throws_WhenUserToFollow_DoesNotExist()
+        {
+            var options = new DbContextOptionsBuilder<DatabaseContext>()
+                .UseInMemoryDatabase(nameof(RemoveFollower_UsingUsername_Throws_WhenUserToFollow_DoesNotExist))
+                .Options;
+            
+            await using (var dbContext = new DatabaseContext(options))
+            {
+                await Assert.ThrowsAsync<InvalidOperationException>(async () => 
+                {
+                    var service = new UserService(dbContext);
+
+                    await service.RemoveFollower("a", "b", CancellationToken.None);
+                });
+            }
+        }
+        
+        [Fact]
+        public async Task RemoveFollower_UsingUsername_Throws_WhenUserFollowing_DoesNotExist()
+        {
+            var options = new DbContextOptionsBuilder<DatabaseContext>()
+                .UseInMemoryDatabase(nameof(RemoveFollower_UsingUsername_Throws_WhenUserFollowing_DoesNotExist))
+                .Options;
+
+            // Seeding the database
+            await using (var dbContext = new DatabaseContext(options))
+            {
+                dbContext.Users.Add(new User { Username = "a", Email = "a@a.a", Password = "a" });
+                dbContext.SaveChanges();
+            }
+
+            await using (var dbContext = new DatabaseContext(options))
+            {
+                await Assert.ThrowsAsync<InvalidOperationException>(async () => 
+                {
+                    var service = new UserService(dbContext);
+
+                    await service.RemoveFollower("a", "b", CancellationToken.None);
+                });
+            }
+        }
+        
+        [Fact]
+        public async Task RemoveFollower_UsingUsername_Throws_WhenFollowerRelation_DoesNotExist()
+        {
+            var options = new DbContextOptionsBuilder<DatabaseContext>()
+                .UseInMemoryDatabase(nameof(RemoveFollower_UsingUsername_Throws_WhenFollowerRelation_DoesNotExist))
+                .Options;
+
+            // Seeding the database
+            await using (var dbContext = new DatabaseContext(options))
+            {
+                dbContext.Users.Add(new User { Username = "a", Email = "a@a.a", Password = "a" });
+                dbContext.Users.Add(new User { Username = "b", Email = "b@b.b", Password = "b" });
+                
+                dbContext.SaveChanges();
+            }
+
+            await using (var dbContext = new DatabaseContext(options))
+            {
+                await Assert.ThrowsAsync<InvalidOperationException>(async () => 
+                {
+                    var service = new UserService(dbContext);
+
+                    await service.RemoveFollower("a", "b", CancellationToken.None);
+                });
+            }
+        }
+        
+        [Fact]
+        public async Task RemoveFollower_UsingUserID_Works()
+        {
+            var options = new DbContextOptionsBuilder<DatabaseContext>()
+                .UseInMemoryDatabase(nameof(RemoveFollower_UsingUserID_Works))
+                .Options;
+
+            // Seeding the database
+            await using (var dbContext = new DatabaseContext(options))
+            {
+                dbContext.Users.Add(new User { Username = "a", Email = "a@a.a", Password = "a" });
+                dbContext.Users.Add(new User { Username = "b", Email = "b@b.b", Password = "b" });
+                
+                dbContext.Followers.Add(new Follower { WhoID = 2, WhomID = 1 });
+                
+                dbContext.SaveChanges();
+            }
+
+            await using (var dbContext = new DatabaseContext(options))
+            {
+                var service = new UserService(dbContext);
+                
+                await service.RemoveFollower(1, "b", CancellationToken.None);
+            }
+            
+            await using (var dbContext = new DatabaseContext(options))
+            {
+                var followers = dbContext.Followers.Where(row => row.WhomID == 1).ToList();
+                
+                Assert.Empty(followers);
+            }
+        }
+        
+        [Fact]
+        public async Task RemoveFollower_UsingUserID_Throws_WhenUserToFollow_DoesNotExist()
+        {
+            var options = new DbContextOptionsBuilder<DatabaseContext>()
+                .UseInMemoryDatabase(nameof(RemoveFollower_UsingUserID_Throws_WhenUserToFollow_DoesNotExist))
+                .Options;
+
+            await using (var dbContext = new DatabaseContext(options))
+            {
+                await Assert.ThrowsAsync<InvalidOperationException>(async () => 
+                {
+                    var service = new UserService(dbContext);
+
+                    await service.RemoveFollower(1, "b", CancellationToken.None);
+                });
+            }
+        }
+        
+        [Fact]
+        public async Task RemoveFollower_UsingUserID_Throws_WhenUserFollowing_DoesNotExist()
+        {
+            var options = new DbContextOptionsBuilder<DatabaseContext>()
+                .UseInMemoryDatabase(nameof(RemoveFollower_UsingUserID_Throws_WhenUserFollowing_DoesNotExist))
+                .Options;
+
+            // Seeding the database
+            await using (var dbContext = new DatabaseContext(options))
+            {
+                dbContext.Users.Add(new User { Username = "a", Email = "a@a.a", Password = "a" });
+
+                dbContext.SaveChanges();
+            }
+
+            await using (var dbContext = new DatabaseContext(options))
+            {
+                await Assert.ThrowsAsync<InvalidOperationException>(async () => 
+                {
+                    var service = new UserService(dbContext);
+
+                    await service.RemoveFollower(1, "b", CancellationToken.None);
+                });
+            }
+        }
+        
+        [Fact]
+        public async Task RemoveFollower_UsingUserID_Throws_WhenFollowerRelation_DoesNotExist()
+        {
+            var options = new DbContextOptionsBuilder<DatabaseContext>()
+                .UseInMemoryDatabase(nameof(RemoveFollower_UsingUserID_Throws_WhenFollowerRelation_DoesNotExist))
+                .Options;
+
+            // Seeding the database
+            await using (var dbContext = new DatabaseContext(options))
+            {
+                dbContext.Users.Add(new User { Username = "a", Email = "a@a.a", Password = "a" });
+                dbContext.Users.Add(new User { Username = "b", Email = "b@b.b", Password = "b" });
+
+                dbContext.SaveChanges();
+            }
+
+            await using (var dbContext = new DatabaseContext(options))
+            {
+                await Assert.ThrowsAsync<InvalidOperationException>(async () => 
+                {
+                    var service = new UserService(dbContext);
+
+                    await service.RemoveFollower(1, "b", CancellationToken.None);
+                });
             }
         }
     }
