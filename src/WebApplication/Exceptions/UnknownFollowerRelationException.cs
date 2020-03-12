09cf4b4174de76cf63a59442543afd4f29af0347
Thomas <Trivivium@users.noreply.github.com>
2020-03-12 18:51:01 +0100

    Remove unnecessary $ signs in exception message

--- src/WebApplication/Exceptions/UnknownFollowerRelationException.cs
+++ src/WebApplication/Exceptions/UnknownFollowerRelationException.cs
@@ -5,7 +5,7 @@ namespace WebApplication.Exceptions
     public class UnknownFollowerRelationException : Exception
     {
         public UnknownFollowerRelationException(int userToFollowID, int userFollowingID) 
-            : base($"Unknown follower relation between users with IDs ${userToFollowID} (whom) and ${userToFollowID} (who).")
+            : base($"Unknown follower relation between users with IDs {userToFollowID} (whom) and {userToFollowID} (who).")
         {}
     }
 }
