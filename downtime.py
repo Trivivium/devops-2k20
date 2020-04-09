d0c5235d92ba6bdb398b0ce0a2fb3b837bf71319
Gustav Winberg <gustavwin@gmail.com>
2020-04-09 15:34:54 +0200

    added monitoring and calculation of metrics

---
+++ downtime.py
@@ -0,0 +1,57 @@
+import csv
+import datetime
+import numpy as np
+
+def main():
+    get_downtime()
+
+def get_downtime():
+    start = None
+    end = None
+    prev = True
+
+    downtimes = []
+    response_times = []
+
+    with open('log.csv', 'r') as logfile:
+        reader = csv.DictReader(logfile)
+        
+        for record in reader:
+            if(start == None or start > str_to_date(record['timestamp'])):
+                start = str_to_date(record['timestamp'])
+            if(end == None or end < str_to_date(record['timestamp'])):
+                end = str_to_date(record['timestamp'])
+
+            try:
+                if (record['status'] is '0' and prev == True):
+                    down = str_to_date(record['timestamp'])
+                    prev = False
+                elif (record['status'] is '1' and prev == False):
+                    up = str_to_date(record['timestamp'])
+                    downtimes.append((up - down).total_seconds())
+                    prev = True
+
+                    response_times.append(float(record['response_time']))
+                elif (record['status'] is '1'):
+                    response_times.append(float(record['response_time']))
+
+            except Exception as e:
+                print (str(e))
+    
+    if (len(downtimes) == 0):
+        total_uptime = 1
+        average_recovery_time = 0
+    else:
+        average_recovery_time = np.mean(downtimes)
+        total_uptime = (1-np.sum(downtimes)/((end-start).total_seconds()))
+
+    average_response_time = np.mean(response_times)
+
+    print (total_uptime, average_recovery_time, average_response_time)
+    return (total_uptime, average_response_time, average_recovery_time) 
+
+def str_to_date(timestamp):
+    return datetime.datetime.strptime(timestamp, "%Y-%m-%d %H:%M:%S")
+
+if __name__ == "__main__":
+    main()
---
+++ monitor.py
@@ -0,0 +1,56 @@
+import requests
+import os
+import csv
+import time
+import datetime
+import sys
+
+def main():
+    WEBPAGE = 'http://minitwit.dk/'
+
+    if (not log_exists()):
+            create_log()
+            print ("created log")
+    monitor(WEBPAGE)   
+    
+def monitor(webpage):
+    while(True):
+        try:
+            response = requests.get(webpage)
+            print (int(response.status_code))
+            print (response.elapsed.total_seconds())
+            if response.status_code == requests.codes.ok:
+                response_time = response.elapsed.total_seconds()
+                print ("alive")
+                append_to_file(1, response_time)
+            else:
+                print ("dead")
+                append_to_file(0)
+            time.sleep(1)
+        except Exception as e:
+            print ("no internet")
+            print (str(e))
+
+def current_timestamp():
+    return datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+def str_to_date(timestamp):
+    return datetime.datetime.strptime(timestamp, "%Y-%m-%d %H:%M:%S")
+            
+def log_exists():
+    return os.path.isfile('log.csv')
+
+def create_log():
+    with open('log.csv', 'a') as log_file:
+        columns = ['timestamp', 'status', 'response_time']
+        writer = csv.DictWriter(log_file, fieldnames = columns)
+        writer.writeheader()
+   
+def append_to_file(status, response_time=1000):
+    with open('log.csv', 'a') as log_file:
+        columns = ['timestamp', 'status', 'response_time']
+        writer = csv.DictWriter(log_file, fieldnames=columns)
+        writer.writerow({'timestamp': str(current_timestamp()), 'status': status, 'response_time': response_time})
+
+if __name__ == "__main__":
+    main()
